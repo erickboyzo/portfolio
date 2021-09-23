997c3ddffca813f43a7d0d26a28b9079116d122c
Erick Boyzo <erickb59@gmail.com>
2021-09-22 20:54:51 -0400

    feat(Contact Me): complete ContactMe.vue view

--- src/views/ContactMe.vue
+++ src/views/ContactMe.vue
@@ -1,32 +1,30 @@
 <template>
-
   <v-row no-gutters>
     <v-col v-if="sent" cols="12 mt-2">
       <v-alert
         dense
         prominent
-        type="success">
-           <strong>Message Delivered!</strong>
-        Thank you for contacting me, I will reach back to you in a short time.
+        type="success" v-html="$Constants.MESSAGE_SUCCESS">
       </v-alert>
     </v-col>
     <v-col cols="12">
-      <Section>
+      <Section :loading="loading">
         <template v-slot:header>
 
           <div class="d-flex justify-center">
-            <h3 class="font-weight-regular">Contact Me</h3>
+            <h3 class="font-weight-regular">{{ $Constants.CONTACT_ME }}</h3>
           </div>
 
         </template>
         <template v-slot:content>
           <form method="post"
                 @submit.prevent="handleSubmit">
-            <input type="hidden" name="form-name" value="contact" />
+            <input type="hidden" name="form-name" value="contact"/>
             <v-text-field
               name="name"
               v-model="form.name"
-              label="Your Name"
+              label="Full Name"
+              prepend-inner-icon="mdi-account"
               outlined
               clearable
               required
@@ -35,37 +33,42 @@
               name="email"
               v-model="form.email"
               label="Email"
+              prepend-inner-icon="mdi-at"
               type="email"
               outlined
               clearable
               required
             />
-          <v-text-field
-            name="subject"
-            v-model="form.subject "
-            label="Subject"
-            outlined
-            clearable
-            required
-          />
+            <v-text-field
+              name="subject"
+              v-model="form.subject "
+              label="Subject"
+              prepend-inner-icon="mdi-email-edit"
+              outlined
+              clearable
+              required
+            />
             <v-textarea
               name="message"
               v-model="form.message"
               label="Message"
+              prepend-inner-icon="mdi-message-text"
               outlined
               clearable
               required
             />
             <div class="d-flex justify-end">
-              <v-btn x-large type="submit" color="primary"><v-icon>mdi-send</v-icon></v-btn>
+              <v-btn x-large type="submit" rounded elevation="4"
+                     color="primary">Send Message
+                <v-icon class="ml-3">mdi-send</v-icon>
+              </v-btn>
             </div>
-        </form>
+          </form>
         </template>
-        <template v-slot:actions>  </template>
+        <template v-slot:actions></template>
       </Section>
     </v-col>
   </v-row>
-
 </template>
 
 <script>
@@ -83,6 +86,7 @@ export default {
       message: '',
     },
     sent: false,
+    loading: false,
   }),
   methods: {
     resetForm() {
@@ -98,6 +102,7 @@ export default {
         .join('&');
     },
     handleSubmit() {
+      this.loading = true;
       fetch('/', {
         method: 'POST',
         headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
@@ -107,16 +112,15 @@ export default {
         }),
       })
         .then(() => {
-          console.log('Success!');
           this.sent = true;
           this.resetForm();
           setTimeout(() => {
             this.sent = false;
-          }, 3000);
+          }, 8000);
         })
         .catch(() => {
           console.log('Error!');
-        });
+        }).finally(() => { this.loading = false; });
     },
   },
 };
