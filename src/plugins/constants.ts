d252704ce5573a9969e93f1b3ba980787a9a4d6c
Erick Boyzo <erickb59@gmail.com>
2021-09-29 16:14:55 -0400

    feat(App Constant): add more static verbiage to constants.ts

--- src/plugins/constants.ts
+++ src/plugins/constants.ts
@@ -8,29 +8,25 @@ const Constants = {
       VIEW_PROJECTS: 'View Projects',
       PROJECTS: 'Projects & Skills',
       ABOUT_PERSONAL_DETAILS: 'Personal Details',
+      DEV_EXPERIENCE: 'Development Experience',
+      TECH_SKILLS: 'Technical Skills',
+      SHOW_MORE: 'Show More',
+      SHOW_LESS: 'Show Less',
+      DESCRIPTION: 'Description',
+      PROJECT_TAGS: 'Project Tags',
+      VIEW_SOURCE: 'View Source Code',
+      GO_TO_APP: 'Go to app',
+      SEND_MESSAGE: 'Send Message',
       SOCIAL_ICONS: [
         { icon: 'devicon-linkedin-plain', link: 'https://www.linkedin.com/in/erick-boyzo-258023a1' },
         { icon: 'devicon-github-original', link: 'https://github.com/erickboyzo' }],
       ABOUT_ME: 'About Me',
-      ABOUT_CONTENT: 'Hello there! I\'m <strong>Erick Boyzo</strong>,\n'
-        + '                    <br>\n'
-        + '                    <br>\n'
-        + '                    Enthusiastic Front-End Developer with a passion for creating engaging and\n'
-        + '                    user-friendly web applications that simplify our daily experiences and tasks.\n'
-        + '                    <br>\n'
-        + '                    <br>\n'
-        + '                    While my domain of expertise is Front-End Development,\n'
-        + '                    I genuinely enjoy exploring Full-Stack Development,\n'
-        + '                    Mobile Application Development, and Automation.\n'
-        + '                    It\'s seeing an idea come to fruition or providing a solution to\n'
-        + '                    an existing\n'
-        + '                    problem, that I appreciate most about software development and what keeps\n'
-        + '                    me motivated.\n'
-        + '                    <br>\n'
-        + '                    <br>\n'
-        + '                    Have an idea you want to collaborate on?\n'
-        + '                    Want to provide some feedback or just want to say hello?\n'
-        + '                    Don\'t be shy, please feel free to send me a message.',
+      ABOUT_CONTENT: 'Hello there! </br></br> I\'m <strong>Erick Boyzo</strong>,\n'
+        + 'an enthusiastic Front-End Developer. In my role as a Front-End Developer, I strive to create innovative and engaging web applications that simplify our daily lives.\n'
+        + '\n </br> </br>'
+        + 'As a hobby, I genuinely enjoy Full-Stack Development, Mobile Application Development, and Automation. My favorite part of software development involves seeing ideas come to life or providing solutions to existing problems.\n'
+        + '\n </br> </br>'
+        + 'Want to collaborate on an idea? Want to provide feedback? Or just wanted to say hello? Don\'t be shy, please feel free to message me.',
       ABOUT_ICONS: [
         { icon: 'mdi-map-marker', label: 'Residence', value: 'Atlanta, GA, United States' },
         {
@@ -38,31 +34,32 @@ const Constants = {
         },
         { icon: 'mdi-account-details', label: 'Occupation', value: 'Front-End Developer' }],
       TOPIC_ICONS: [
-        { icon: 'devicon-angularjs-plain colored', value: ['angular', 'angular8'], experience: 90 },
-        { icon: 'devicon-react-original colored', value: ['react'], experience: 80 },
-        { icon: 'devicon-android-plain-wordmark colored', value: ['android'], experience: 75 },
-        { icon: 'devicon-bootstrap-plain colored', value: ['bootstrap', 'Bootstrap'] },
-        { icon: 'devicon-express-original colored', value: ['express'] },
-        { icon: 'devicon-firebase-plain colored', value: ['firebase'] },
-        { icon: 'devicon-gradle-plain-wordmark colored', value: ['gradle'] },
-        { icon: 'devicon-heroku-original-wordmark colored', value: ['heroku'] },
-        { icon: 'devicon-html5-plain-wordmark colored', value: ['html5', 'html'], experience: 98 },
-        { icon: 'devicon-java-plain-wordmark colored', value: ['java'], experience: 80 },
-        { icon: 'devicon-javascript-plain colored', value: ['javascript'], experience: 95 },
-        { icon: 'devicon-less-plain-wordmark colored', value: ['less'], experience: 97 },
-        { icon: 'devicon-nodejs-plain-wordmark colored', value: ['node', 'nodejs'], experience: 50 },
-        { icon: 'devicon-php-plain colored', value: ['php'], experience: 75 },
-        { icon: 'devicon-postgresql-plain-wordmark colored', value: ['postgresql'] },
-        { icon: 'devicon-mysql-plain colored', value: ['mysql'], experience: 50 },
-        { icon: 'devicon-python-plain-wordmark colored', value: ['python'], experience: 75 },
-        { icon: 'devicon-rails-plain-wordmark colored', value: ['rails'] },
-        { icon: 'devicon-ruby-plain colored', value: ['ruby'], experience: 50 },
-        { icon: 'devicon-sass-original colored', value: ['sass'], experience: 97 },
-        { icon: 'devicon-typescript-plain colored', value: ['typescript'], experience: 95 },
-        { icon: 'devicon-vuejs-plain-wordmark colored', value: ['vue', 'vuejs'], experience: 90 }],
+        { icon: 'devicon-angularjs-plain colored', displayLabel: 'Angular', value: ['angular', 'angular8'], experience: 90 },
+        { icon: 'devicon-react-original colored', displayLabel: 'React', value: ['react'], experience: 80 },
+        { icon: 'devicon-android-plain-wordmark colored', displayLabel: 'Android', value: ['android'], experience: 75 },
+        { icon: 'devicon-bootstrap-plain colored', displayLabel: 'Bootstrap', value: ['bootstrap', 'Bootstrap'] },
+        { icon: 'devicon-express-original colored', displayLabel: 'Express', value: ['express'] },
+        { icon: 'devicon-firebase-plain colored', displayLabel: 'Firebase', value: ['firebase'] },
+        { icon: 'devicon-gradle-plain-wordmark colored', displayLabel: 'Gradle', value: ['gradle'] },
+        { icon: 'devicon-heroku-original-wordmark colored', displayLabel: 'Heroku', value: ['heroku'] },
+        { icon: 'devicon-html5-plain-wordmark colored', displayLabel: 'HTML5', value: ['html5', 'html'], experience: 95 },
+        { icon: 'devicon-java-plain-wordmark colored', displayLabel: 'Java', value: ['java'], experience: 80 },
+        { icon: 'devicon-spring-plain colored', displayLabel: 'Spring', value: ['spring'] },
+        { icon: 'devicon-javascript-plain colored', displayLabel: 'Javascript', value: ['javascript'], experience: 95 },
+        { icon: 'devicon-less-plain-wordmark colored', displayLabel: 'Less', value: ['less'], experience: 95 },
+        { icon: 'devicon-nodejs-plain-wordmark colored', displayLabel: 'Node.js', value: ['node', 'nodejs'], experience: 50 },
+        { icon: 'devicon-php-plain colored', value: ['php'], displayLabel: 'PHP', experience: 75 },
+        { icon: 'devicon-postgresql-plain-wordmark colored', displayLabel: 'PostgreSQL', value: ['postgresql'] },
+        { icon: 'devicon-mysql-plain colored', value: ['mysql'], displayLabel: 'MySQL', experience: 50 },
+        { icon: 'devicon-python-plain-wordmark colored', displayLabel: 'Python', value: ['python'], experience: 75 },
+        { icon: 'devicon-rails-plain-wordmark colored', displayLabel: 'Rails', value: ['rails'] },
+        { icon: 'devicon-ruby-plain colored', displayLabel: 'Ruby', value: ['ruby'], experience: 50 },
+        { icon: 'devicon-sass-original colored', displayLabel: 'Sass', value: ['sass'], experience: 95 },
+        { icon: 'devicon-typescript-plain colored', displayLabel: 'Typescript', value: ['typescript'], experience: 90 },
+        { icon: 'devicon-vuejs-plain-wordmark colored', displayLabel: 'Vue', value: ['vue', 'vuejs'], experience: 90 }],
       PROFESSIONAL_SKILLS: [
-        { icon: 'mdi-television-guide', label: 'UI Development', experience: 85 },
-        { icon: 'mdi-monitor', label: 'Web Application Development', experience: 90 },
+        { icon: 'mdi-television-guide', label: 'UI Development', experience: 90 },
+        { icon: 'mdi-monitor', label: 'Web Application Development', experience: 95 },
         { icon: 'mdi-tablet-cellphone', label: 'Mobile Application Development', experience: 80 },
         { icon: 'mdi-cogs', label: 'Automation', experience: 70 },
       ],
