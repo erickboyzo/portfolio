7e8ef20ce047486bc73240c4dd882552b9dab790
Erick Boyzo <erickb59@gmail.com>
2021-09-29 16:17:14 -0400

    feat(Icon Links): add ability to make icon color as primary

--- src/components/IconLinks.vue
+++ src/components/IconLinks.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <v-btn class="icon-link" v-for="(button, index) of $Constants.SOCIAL_ICONS"
+           :color="primary ? 'primary': null"
            v-bind:key="index"
            icon x-large :href="button.link" target="_blank"><i :class="button.icon"></i></v-btn>
   </div>
@@ -9,6 +10,7 @@
 <script>
 export default {
   name: 'IconLinks',
+  props: ['primary'],
 };
 </script>
 
