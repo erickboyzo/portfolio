5cd878c16ca6c424cc96aee70bf0749f607807b3
Erick Boyzo <erick.boyzo@gmail.com>
2025-04-28 23:19:32 -0400

    feature(app): update App.vue to Composition API

--- src/App.vue
+++ src/App.vue
@@ -2,81 +2,86 @@
   <v-app id="portfolio">
     <template v-if="!loading">
       <Header :flush-header="isAtTheTopOfPage">
-        <RouterView/>
+        <RouterView />
       </Header>
-      <Footer/>
+      <Footer />
     </template>
-    <v-dialog v-if="loading" max-width="320" v-model="loading" persistent>
-      <v-list class="py-2" color="primary" elevation="12" rounded="lg">
-        <v-list-item title="Loading resume...">
-          <template v-slot:prepend>
-            <div class="pe-4">
-              <v-icon color="primary" size="x-large"></v-icon>
-            </div>
-          </template>
-
-          <template v-slot:append>
-            <v-progress-circular color="primary" indeterminate="disable-shrink" size="24" width="2"></v-progress-circular>
-          </template>
-        </v-list-item>
-      </v-list>
-    </v-dialog>
+    <LoadingDialog :show="loading" :error="apiError" @retry="fetchProfile" />
   </v-app>
 </template>
 
 <script setup lang="ts">
-import { resumeStore } from '@/stores/store';
-import axios from 'axios';
-import debounce from 'lodash/debounce';
-import { onBeforeMount, onMounted, onUnmounted, shallowRef } from 'vue';
-import { useTheme } from 'vuetify/dist/vuetify';
-import Footer from './components/Footer.vue';
-import Header from './components/Header.vue';
+import LoadingDialog from "@/components/LoadingDialog.vue";
+import type { ProfileResponse } from "@/interfaces/profile-response";
+import { useResumeStore } from "@/stores/store";
+import axios from "axios";
+import debounce from "lodash/debounce";
+import { onBeforeMount, onMounted, onUnmounted, ref } from "vue";
+import { useTheme } from "vuetify";
+import Footer from "./components/Footer.vue";
+import Header from "./components/Header.vue";
+
+const useScrollHandler = () => {
+  const isAtTheTopOfPage = ref(true);
+  let handleDebouncedScroll: Function;
+
+  const handleScroll = () => {
+    isAtTheTopOfPage.value = !window.pageYOffset;
+  };
+
+  onMounted(() => {
+    handleDebouncedScroll = debounce(handleScroll, 100);
+    window.addEventListener("scroll", handleDebouncedScroll);
+  });
+
+  onUnmounted(() => {
+    window.removeEventListener("scroll", handleDebouncedScroll);
+  });
+
+  return { isAtTheTopOfPage };
+};
 
 const theme = useTheme();
-const store = resumeStore();
-const loading = shallowRef(true);
-let handleDebouncedScroll;
-const isAtTheTopOfPage = shallowRef(true);
+const store = useResumeStore();
+const loading = ref(true);
+const apiError = ref(false);
+const { isAtTheTopOfPage } = useScrollHandler();
 
-function handleScroll() {
-  isAtTheTopOfPage.value = !window.pageYOffset;
-}
-
-onMounted(() => {
-  theme.global.name.value = localStorage.getItem('darkTheme') === 'true' ? 'dark' : 'light';
-  handleDebouncedScroll = debounce(handleScroll, 100);
-  window.addEventListener('scroll', handleScroll);
-});
-
-onUnmounted(() => {
-  window.removeEventListener('scroll', handleDebouncedScroll);
-});
-
-function fetchProfile() {
+const fetchProfile = async () => {
   loading.value = true;
-  axios
-    .get(`https://gitconnected.com/v1/portfolio/${store.gitConnectedProfileKey}`)
-    .then((response) => {
-      store.$patch({
-        resumeData: response.data,
-      });
-    })
-    .catch((error) => console.error(error))
-    .finally(() => {
-      loading.value = false;
+  apiError.value = false;
+
+  try {
+    const response = await axios.post("portfolio", { gitConnectedProfileKey: store.gitConnectedProfileKey }, { headers: { "Content-Type": "application/json" } });
+
+    const { profile, siteMetaData } = response.data as ProfileResponse;
+    store.$patch((state) => {
+      state.resumeData = profile;
+      state.metaData = siteMetaData;
     });
-}
+    loading.value = false;
+    apiError.value = false;
+  } catch (error) {
+    apiError.value = true;
+  }
+};
 
 onBeforeMount(() => {
+  theme.global.name.value = localStorage.getItem("darkTheme") === "true" ? "dark" : "light";
   fetchProfile();
 });
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 @use "node_modules/vuetify/settings";
 @import "node_modules/vuetify/styles";
 @import "theme";
+
+:root {
+  --scrollbar-width: 0.5rem;
+  --scrollbar-radius: 0.5rem;
+  --scrollbar-shadow: inset 0 0 0.5rem rgba(0, 0, 0, 0.1);
+}
 
 html {
   overflow-y: auto;
@@ -85,19 +90,19 @@ html {
 }
 
 ::-webkit-scrollbar {
-  width: 0.5rem;
-  height: 0.5rem;
+  width: var(--scrollbar-width);
+  height: var(--scrollbar-width);
 }
 
 ::-webkit-scrollbar-track {
-  box-shadow: inset 0 0 0.5rem rgba(0, 0, 0, 0.1);
+  box-shadow: var(--scrollbar-shadow);
   background-color: map-get(settings.$grey, "lighten-2");
 }
 
 ::-webkit-scrollbar-thumb {
-  border-radius: 0.5rem;
+  border-radius: var(--scrollbar-radius);
   background-color: map-get(settings.$grey, "base");
-  box-shadow: inset 0 0 0.5rem rgba(0, 0, 0, 0.1);
+  box-shadow: var(--scrollbar-shadow);
 }
 
 .fade-enter-active,
