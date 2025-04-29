288b7e56176f3b0d06539b3555a33f667cb794f6
Erick Boyzo <erick.boyzo@gmail.com>
2025-04-28 23:22:27 -0400

    feature(Footer): update Footer.vue to Composition API

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,12 +1,11 @@
 <template>
-  <v-footer absolute app>
+  <v-footer app absolute>
     <v-container id="footer-container" class="fill-height footer-container">
       <v-row>
         <v-col cols="12" class="d-flex justify-center py-1 mt-3">
-          <IconLinks :primary="theme.global.current.value.dark"></IconLinks>
+          <IconLinks :primary="isDarkTheme" />
         </v-col>
-        <v-col cols="12" class="d-flex justify-center py-1 my-1"> &copy; {{ currentYear }} {{ copyRight }}</v-col>
-        <!--        <v-col cols="12" class="d-flex justify-center py-1 my-1"> This portfolio was build using the following technologies. </v-col>-->
+        <v-col cols="12" class="d-flex justify-center py-1 my-1"> &copy; {{ currentYear }} {{ copyRight }} </v-col>
       </v-row>
     </v-container>
   </v-footer>
@@ -14,26 +13,28 @@
 
 <script setup lang="ts">
 import IconLinks from "@/components/IconLinks.vue";
-import { resumeStore } from "@/stores/store";
-import { useTheme } from "vuetify/dist/vuetify";
+import { useResumeStore } from "@/stores/store";
+import { computed } from "vue";
+import { useTheme } from "vuetify";
 
-const currentYear = new Date().getFullYear();
-const copyRight = resumeStore().resume.basics.name;
 const theme = useTheme();
+const isDarkTheme = computed(() => theme.global.current.value.dark);
+const currentYear = new Date().getFullYear();
+const copyRight = useResumeStore().resume.basics.name;
 </script>
 
-<style lang="scss" scoped>
-.icon-link {
-  i {
-    font-size: 28px;
-  }
-}
-
+<style lang="scss">
 .v-footer {
   background-color: rgb(var(--v-theme-background));
 
   .footer-container {
-    border-top: 0.2px solid rgb(var(--v-theme-outline));
+    border-top: 1px solid rgb(var(--v-theme-outline));
+  }
+
+  .icon-link {
+    i {
+      font-size: 1.75rem;
+    }
   }
 }
 </style>
